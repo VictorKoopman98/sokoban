48f359df0b302554edb0a868163c376af0f98157
LennardGeeraert <lennard.geeraert@student.hogent.be>
2020-03-23 23:23:40 +0100

    klein detail toegevoegd

--- src/cui/SokobanApplicatie.java
+++ src/cui/SokobanApplicatie.java
@@ -49,6 +49,7 @@ public class SokobanApplicatie
 			if(keuze == 1 || keuze == 2)  //indien aangemeld of geregistreerd toon de spellen en opties om spellen en spelborden te voltooien
 			{
 				new UC3Test(domeincontroller).kiesSpel();
+				
 			}
 			
 		} while(keuze != 3);
