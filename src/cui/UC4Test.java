e55ecdb424bb5e36a309039207d9c60c805efef6
LennardGeeraert <lennard.geeraert@student.hogent.be>
2020-03-31 17:13:38 +0200

    ercrz

--- src/cui/UC4Test.java
+++ src/cui/UC4Test.java
@@ -26,6 +26,7 @@ public class UC4Test
 		dc.toonSpelbord();  //spelbord tonen
 		
 		int actie = toonActiesSpelbord();
+		
 		do {
 			
 			
@@ -61,18 +62,19 @@ public class UC4Test
 					
 					dc.toonSpelbord();
 					
-					actie = toonActiesSpelbord();  //verichte keuze in toonActiesSpelbord omztten naar "actie"
+					if(!dc.eindeSpelbordBereikt())
+					{
+						actie = toonActiesSpelbord();  //verichte keuze in toonActiesSpelbord omztten naar "actie"
+					}
 
 				}
-				while(!dc.eindeSpelbordBereikt() || actie != 3 || actie != 2);   //blijf verplaatsen tot dat einde spelbord bereikt is
+				while(!dc.eindeSpelbordBereikt() && actie != 3 && actie != 2);   //blijf verplaatsen tot dat einde spelbord bereikt is
 			}
 			else if(actie == 3)   //spelbord verlaten
 			{
 				System.out.printf("%s heeft het spelbord verlaten", dc.geefGebruikersnaam());
 			}
-		}while(!dc.eindeSpelbordBereikt() || actie != 3 || actie != 2);
-		
-		
+		}while(!dc.eindeSpelbordBereikt() || actie != 3);
 	}
 	
 	
